9f5e2f6a43fb1427b8a9ff43696695383929a14c
olivia <oliviaw2026@student.cis.edu.hk>
2024-02-29 12:38:17 +0800

    yay!

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> previousListeners = new ArrayList<String>();
+
+    public Song(String title, String artist) {
+        this.title=title;
+        this.artist=artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+       int noOfNewListeners=0;
+       for (String listener: listeners) {
+           if (!previousListeners.contains(listener.toLowerCase())) {
+               previousListeners.add(listener.toLowerCase());
+               noOfNewListeners++;
+           }
+       }
+       return noOfNewListeners;
+    }
 }
